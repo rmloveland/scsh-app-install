8ad80e0bb6226448a1968244a90e56581584f7f0
Rich Loveland <r@rmloveland.com>
2013-09-18 09:52:22 -0400

    Now linking the user binary into the app dir.
    Note: I've been going to a lot of trouble to get the scsh VM into the
    app directory, but this turns out to have been a waste of time, since
    the VM itself uses dynamically linked system libraries. This means
    that I can't deploy a static binary to users (They will have to
    install Scsh on their systems somehow. Shit.) Therefore, the next step
    is to investigate building Scsh as a static binary.

--- main.scm
+++ main.scm
@@ -9,10 +9,15 @@
 ;;--------------------------------------------------------------------
 ;; Procedures.
 
-(define (make-app-dir-name name)
+(define (make-user-bin-dir-name)
   (string-append
    (home-dir)
-   "/bin/."
+   "/bin"))
+
+(define (make-app-dir-name name)
+  (string-append
+   (make-user-bin-dir-name)
+   "/."
    name))
 
 (define (make-app-image-name app-dir name)
@@ -37,18 +42,34 @@
 EOF
 ))
     (dynamic-wind
-      ;; SEEK doesn't handle buffered ports.
+      ;; We need to turn off buffering because Scsh's implementation
+      ;; of SEEK can't handle buffered I/O ports.
       (lambda () (set-port-buffering output-port bufpol/none))
       (lambda () (begin
 		   (seek output-port 0)
 		   (display string output-port)))
       (lambda () (close output-port)))))
 
+(define (make-user-binary-name app-name)
+  (string-append (make-user-bin-dir-name) "/" app-name))
+
+(define (link-app-image-as-user-binary app-image-name user-binary-name)
+  (run (ln -s ,app-image-name ,user-binary-name)))
+
+(define (set-file-executable! f)
+  (set-file-mode 
+   f
+   (bitwise-ior 755 (file-mode f))))
+
+(define set-user-binary-executable! set-file-executable!)
+
 (define (deploy-app name)
-  (begin
-    (let* ((app-dir-name (make-app-dir-name name))
-	   (app-image-name (make-app-image-name app-dir-name name)))
-      (begin
-	(create-app-dir app-dir-name)
-	(copy-vm-to-app-dir app-dir-name)
-	(dump-app-image app-image-name)))))
+  (let* ((app-dir-name (make-app-dir-name name))
+	 (app-image-name (make-app-image-name app-dir-name name))
+	 (user-binary-name (make-user-binary-name name)))
+    (begin
+      (create-app-dir app-dir-name)
+      (copy-vm-to-app-dir app-dir-name)
+      (dump-app-image app-image-name)
+      (link-app-image-as-user-binary app-image-name user-binary-name)
+      (set-user-binary-executable! user-binary-name))))
